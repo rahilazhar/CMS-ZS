57c7d456432b3b75eb86edbf64b4cb97ef145f3c
rahilazhar <124768720+rahilazhar@users.noreply.github.com>
2023-12-11 18:38:41 +0500

    Add files via upload

--- Server/Index.js
+++ Server/Index.js
@@ -20,10 +20,10 @@ dbconnection()
 
 app.use("/api/v1/auth", router);
 
-// app.use(express.static(path.join(__dirname, "public")))
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "public")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 
 PORT = process.env.PORT || 3000
